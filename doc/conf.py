aa3bfe62d94b71f53297fac333d4fc3449410d07
Marina Nelson <mjnelson@wpi.edu>
2024-08-18 11:01:47 -0400

    revert change

--- doc/conf.py
+++ doc/conf.py
@@ -14,11 +14,7 @@ release = '0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "recommonmark"]
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
-}
+extensions = ["myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
