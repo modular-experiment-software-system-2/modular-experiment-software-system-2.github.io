6908a41bb52008d1472c60c44d5bd69eba222cee
Marina Nelson <mjnelson@wpi.edu>
2024-08-18 11:35:34 -0400

    try adding bibtexx_bibfiles with path to mess2 bib

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+bibtex_bibfiles = ['doc/projets/mess2/_citations.bib']
 
 
 # -- Options for HTML output -------------------------------------------------
